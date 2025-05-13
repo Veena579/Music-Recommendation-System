525c5ebc74450579547e9f7536827ba842eb0ab9
Veena579 <2020cse.rn7@svec.edu.in>
2025-05-13 19:11:33 +0100

    added layout

--- app/app.js
+++ app/app.js
@@ -25,7 +25,7 @@ app.use(
 
 // ROUTES
 
-app.get("/", (req, res) => res.render("welcome"));
+app.get("/", (req, res) => res.render("home"));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
 app.get("/music", (req, res) => res.render("music"));
